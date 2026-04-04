74a373085cbe2ffae3316735aa6a797928bae34a
EngArsany <arsanyo2003@gmail.com>
2026-04-05 00:14:26 +0200

    Write SJF algorithm

--- Algorithms/SJF.py
+++ Algorithms/SJF.py
@@ -1,1 +1,20 @@
+from typing import List, Optional
+from Algorithms import Scheduler, Process
 
+class SJFScheduler(Scheduler):
+    def __init__(self):
+        super().__init__("Shortest Job First (Non-Preemptive)")
+
+    def get_next_process(self, current_time) -> Optional[Process]:
+        if not self.current_process:
+            return None
+
+        ready_processes = self.get_arrived_processes(current_time)
+        if not ready_processes:
+            return None
+    
+        # Sort list by burst time
+        # If two processes have same SJT, process the first
+        # If two processes have same arrival_time, process the lower pid
+        ready_processes.sort(key=lambda process: (process.get_burst_time(), process.get_arrival_time(), process.get_pid()))
+        return ready_processes[0]
