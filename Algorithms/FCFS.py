a43369d4a9a0d190dc3c115831edd9b1111743fb
EngArsany <arsanyo2003@gmail.com>
2026-04-04 23:15:34 +0200

    Create FCFS Algorithm

---
+++ Algorithms/FCFS.py
@@ -0,0 +1,15 @@
+from typing import List, Optional
+from Algorithms import Scheduler, Process
+
+class FCFSScheduler(Scheduler):
+    def __init__(self):
+        super().__init__("First-Come, First-Served (FCFS)")
+
+    def get_next_process(self) -> Optional[Process]:
+        ready_processes = self.get_arrived_processes()
+
+        if not ready_processes:
+            return None
+
+        ready_processes.sort(key=lambda process: (process.get_arrival_time(), process.get_pid()))
+        return ready_processes
