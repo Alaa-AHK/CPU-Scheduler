ff471db361849c204e442cb593d798f05e2c6ca5
EngArsany <arsanyo2003@gmail.com>
2026-04-04 23:30:27 +0200

    Fix: wrong method override

--- Algorithms/FCFS.py
+++ Algorithms/FCFS.py
@@ -5,8 +5,8 @@ class FCFSScheduler(Scheduler):
     def __init__(self):
         super().__init__("First-Come, First-Served (FCFS)")
 
-    def get_next_process(self) -> Optional[Process]:
-        ready_processes = self.get_arrived_processes()
+    def get_next_process(self, current_time) -> Optional[Process]:
+        ready_processes = self.get_arrived_processes(current_time)
 
         if not ready_processes:
             return None
