0edb7ba31ca11a534ad28697af2da7932a3dfe19
EngArsany <arsanyo2003@gmail.com>
2026-04-05 00:35:23 +0200

    Write preemptive SJT algorithm (SRTF)

---
+++ Algorithms/SRTF.py
@@ -0,0 +1,17 @@
+from Algorithms import Scheduler, Process
+from typing import Optional
+
+class SRTFScheduler(Scheduler):
+    def __init__(self):
+        super().__init__("Shortest Job First (Preemptive)")
+
+    def get_next_process(self, current_time) -> Optional[Process]:
+        ready_processes = self.arrived_processes(current_time)
+
+        ready_processes.sort(key=lambda process: (process.get_burst_time(), process.get_arrival_time(), process.get_pid()))
+        if not ready_processes:
+            return self.current_process if self.current_process else None
+
+        next_process = ready_processes[0] if (ready_processes[0].burst_time < self.current_process.burst_time) else self.current_process
+        return next_process
+    
